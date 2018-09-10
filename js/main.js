de002cd4350be07ab07babd40d9f3b0519ccebc4
Mirco Mueller <mirco.mueller@posteo.de>
2018-09-10 17:58:44 +0200

    Add partially encrypted mapboxToken

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoibWlyY29tbHIiLCJhIjoiY2prcGI4azVjMWhtMjNvczc5cnF3OGszZyJ9.KR-sBqV79Xpd1NSG2NFxxx',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
